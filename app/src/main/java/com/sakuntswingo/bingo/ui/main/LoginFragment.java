402b2c55a174aecc50ef7bdc4286b8fbee0abf5f
sakuntsmarina <sakuntsmarina8@gmail.com>
2025-03-30 13:13:30 +0400

    Commit 2

---
+++ app/src/main/java/com/sakuntswingo/bingo/ui/main/LoginFragment.java
@@ -0,0 +1,40 @@
+package com.sakuntswingo.bingo.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sakuntswingo.bingo.MainViewModel;
+import com.sakuntswingo.bingo.R;
+
+public class LoginFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
